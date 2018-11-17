869eeabb12a3fedb7d7f1509ed40f1f17fe61c32
Lei Wang <lei.wang.sibet@gmail.com>
2018-11-17 12:15:49 -0500

    brief cnn based on torch.nn in pytorch

--- tutorials/beginner_source/blitz/neural_networks_tutorial.py
+++ tutorials/beginner_source/blitz/neural_networks_tutorial.py
@@ -69,8 +69,7 @@ class Net(nn.Module):
     def num_flat_features(self, x):
         size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
-        for s in size:
-            num_features *= s
+        for s in size: num_features *= s
         return num_features
 
 
