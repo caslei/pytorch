04eafa24105ce6cd5e9c657d0140285e9138f024
Lei Wang <lei.wang.sibet@gmail.com>
2018-11-29 09:55:14 -0500

    pytorch nn Module

--- tutorials/beginner_source/blitz/neural_networks_tutorial.py
+++ tutorials/beginner_source/blitz/neural_networks_tutorial.py
@@ -47,10 +47,12 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # 1 input channel, 6 output channels, 5x5 convolution kernel
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True)
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        # torch.nn.Linear(in_features, out_features, bias=True)
+        self.fc1 = nn.Linear(16*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
