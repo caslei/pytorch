f19b0a3a4996d40f522ef2d6772002fc30fe9061
Lei Wang <lei.wang.sibet@gmail.com>
2018-11-29 09:44:41 -0500

    pytorch example

--- tutorials/beginner_source/blitz/neural_networks_tutorial.py
+++ tutorials/beginner_source/blitz/neural_networks_tutorial.py
@@ -46,8 +46,7 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 5x5 square convolution
-        # kernel
+        # 1 input channel, 6 output channels, 5x5 convolution kernel
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
         # an affine operation: y = Wx + b
